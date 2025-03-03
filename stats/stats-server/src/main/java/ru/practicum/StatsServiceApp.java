2c36a0b17e967ea5a7f9a081f66b0897154f3e7d
agayan <a.agayan95@mail.ru>
2025-03-04 01:00:55 +0400

    fic mistake n11

--- stats/stats-server/src/main/java/ru/practicum/StatsServiceApp.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsServiceApp.java
@@ -2,13 +2,9 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@ConfigurationPropertiesScan
 public class StatsServiceApp {
   public static void main(String[] args) {
     SpringApplication.run(StatsServiceApp.class, args);
