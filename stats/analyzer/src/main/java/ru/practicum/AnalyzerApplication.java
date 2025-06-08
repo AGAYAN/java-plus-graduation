134b68586ad919de5440da9176f7a13f752ff782
agayan <a.agayan95@mail.ru>
2025-06-08 19:22:25 +0400

    Fix: undo project

--- stats/analyzer/src/main/java/ru/practicum/AnalyzerApplication.java
+++ stats/analyzer/src/main/java/ru/practicum/AnalyzerApplication.java
@@ -3,13 +3,23 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import ru.practicum.service.AnalyzeService;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "ru.practicum.repository")
-@EntityScan(basePackages = "ru.practicum.model")
+@EnableFeignClients(basePackages = {"interaction.controller", "ewm.client"})
+@ConfigurationPropertiesScan
+@EnableJpaRepositories("ru.practicum.repository")
+@EnableDiscoveryClient
+@EntityScan("analyzer.model")
 public class AnalyzerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AnalyzerApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApplication.class, args);
+        AnalyzeService analyzer = context.getBean(AnalyzeService.class);
+        analyzer.start();
     }
 }
