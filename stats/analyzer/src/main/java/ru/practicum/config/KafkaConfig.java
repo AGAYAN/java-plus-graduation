194766fce7d64218929509c8e9f92b091ab32e07
agayan <a.agayan95@mail.ru>
2025-06-10 16:34:16 +0400

    Fix: testing

--- stats/analyzer/src/main/java/ru/practicum/config/KafkaConfig.java
+++ stats/analyzer/src/main/java/ru/practicum/config/KafkaConfig.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,35 +20,39 @@ import java.util.Properties;
 public class KafkaConfig {
 
     @Value("${kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    private String server;
+
+    @Value("${kafka.key-serializer}")
+    private String keySerializer;
+
+    @Value("${kafka.value-serializer}")
+    private String valueSerializer;
 
     @Value("${kafka.key-deserializer}")
     private String keyDeserializer;
 
     @Value("${kafka.value-deserializer-similarity}")
-    private String valueDeserializerSimilarity;
+    private String eventDeserializer;
 
     @Value("${kafka.value-deserializer-actions}")
-    private String valueDeserializerActions;
-
-    @Value("${kafka.consumer-analyzer-similarity-group-id}")
-    private String consumerAnalyzerSimilarityGroupId;
+    private String userActionDeserializer;
 
     @Value("${kafka.consumer-analyzer-actions-group-id}")
-    private String consumerAnalyzerActionsGroupId;
+    private String actionsConsumerGroupId;
+
+    @Value("${kafka.consumer-analyzer-similarity-group-id}")
+    private String similarityConsumerGroupId;
 
     @Value("${kafka.auto-offset-reset}")
     private String autoOffsetReset;
 
     private Properties propertiesConsumer() {
-
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializerSimilarity);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerAnalyzerSimilarityGroupId);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, eventDeserializer);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, similarityConsumerGroupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
-
         return props;
     }
 
@@ -57,10 +63,10 @@ public class KafkaConfig {
 
     private Properties propertiesActionsConsumer() {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializer);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializerActions);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerAnalyzerActionsGroupId);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, userActionDeserializer);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, actionsConsumerGroupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         return props;
     }
@@ -68,5 +74,14 @@ public class KafkaConfig {
     @Bean
     public KafkaConsumer<String, UserActionAvro> actionsConsumer() {
         return new KafkaConsumer<>(propertiesActionsConsumer());
+    }
+
+    @Bean
+    public KafkaProducer<String, Object> kafkaProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        return new KafkaProducer<>(props);
     }
 }
